828bd06cb797eb4a70185def16bc8d1328acaecc
matac2020 <69847979+matac2020@users.noreply.github.com>
2022-03-17 15:29:57 +0000

    Update director_based_movie_recommender.py

--- director_based_movie_recommender.py
+++ director_based_movie_recommender.py
@@ -2,46 +2,54 @@ import requests
 import pandas as pd
 import re
 
-
-def recommend_movie(movie_name):
-
-  
-
-  api_key = "API Key is here"
-
-  movie_name = re.sub(r' ', '-',movie_name).lower()
-  movie_name = re.sub(r'[^\w\s]', '-', movie_name)
-
-  movie = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+ api_key+"&query=" + movie_name)
-  step = movie.json()
-  movie_id = []
-  for i in step["results"]:
-    movie_id.append(i["id"])
-  movie_id = "{}".format(movie_id[0])
+api_key = "API Key is here"
 
 
-
-  movie_data = requests.get("https://api.themoviedb.org/3/movie/" +movie_id+ "/credits?api_key=" + api_key)
-  movie_data2 = movie_data.json()
-
-  director_id = [] 
-  for i in movie_data2["crew"]:
-    if i["job"] == "Director": 
-      director_id.append(i["id"])
-  director_id = "{}".format(director_id[0])
-
-  director = requests.get("https://api.themoviedb.org/3/person/"+director_id +"/movie_credits?api_key=" +  api_key)
-  director_json= director.json()
+def get_movie_id(moviee):
     
-  columns = ["title", "point"]
-  df = pd.DataFrame(columns=columns)
+    
+    movie_name = re.sub(r' ', '-',moviee).lower()
+    movie_name = re.sub(r'[^\w\s]', '-', movie_name)
 
-  for i in director_json["crew"]:
-    df.loc[len(df)]=[i['title'],i['vote_average']]
+    movie = requests.get("https://api.themoviedb.org/3/search/movie?api_key="+ api_key+"&query=" + movie_name)
+    step = movie.json()
+    movie_id = []
+    for i in step["results"]:
+        movie_id.append(i["id"])
+    movie_id = "{}".format(movie_id[0])
+    
+    return movie_id
+  
+  
+def get_director_id(id_movie):
+    movie_data = requests.get("https://api.themoviedb.org/3/movie/" +"{}".format(id_movie)+ "/credits?api_key=" + api_key)
+    movie_data2 = movie_data.json()
+
+    director_id = [] 
+    for i in movie_data2["crew"]:
+        if i["job"] == "Director": 
+            director_id.append(i["id"])
+            director_id = "{}".format(director_id[0])
+            
+            return director_id
+          
+          
+def recommend_movie(movie_name):
+    
+    director_id = get_director_id(get_movie_id(movie_name))
+    
+
+    director = requests.get("https://api.themoviedb.org/3/person/"+director_id +"/movie_credits?api_key=" +  api_key)
+    director_json= director.json()
+    
+    columns = ["title", "point"]
+    df = pd.DataFrame(columns=columns)
+
+    for i in director_json["crew"]:
+        df.loc[len(df)]=[i['title'],i['vote_average']]
 
 
-  print(df.sort_values("point",ascending=False).drop_duplicates().head(7))
+    print(df.sort_values("point",ascending=False).drop_duplicates().head(7))
 
 
-
-
+recommend_movie("dankirk")
